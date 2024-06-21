8ec315d3d0164ad8a15e062460f1ddd368e9b802
Dmytro Ukrainets <dmytro.army24@gmail.com>
2024-06-21 21:31:06 +0300

    Add package-info and update Main

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,23 @@
 package org.example;
 
+/**
+ * Основний клас програми.
+ */
 public class Main {
-    public static void main(String[] args) {
+
+    /**
+     * Приватний конструктор, щоб запобігти створенню екземпляра утилітного класу.
+     */
+    private Main() {
+        throw new UnsupportedOperationException("This is a utility class and cannot be instantiated");
+    }
+
+    /**
+     * Основний метод програми.
+     *
+     * @param args аргументи командного рядка
+     */
+    public static void main(final String[] args) {
         System.out.println("Hello world!");
     }
 }
